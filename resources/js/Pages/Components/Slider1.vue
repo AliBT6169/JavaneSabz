2df8ea0c175691a59d907c8eea0955b41bc70a3b
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-01 04:23:58 -0800

    making slider is almost dine

--- resources/js/Pages/Components/Slider1.vue
+++ resources/js/Pages/Components/Slider1.vue
@@ -6,6 +6,7 @@ const X = ref(0);
 const slide = ref(0);
 const translation = ref(0);
 const drag = ref(false);
+
 const slider_show = (movement) => {
     if (movement === "forward") {
         slide.value < 2 ? slide.value++ : slide.value = 0;
@@ -14,25 +15,26 @@ const slider_show = (movement) => {
     }
     translation.value = slide.value * 80;
 }
-document.addEventListener('mouseup',function(){
+document.addEventListener('mouseup', function () {
     drag.value = false;
 });
 const mouse_downed = (event) => {
-    console.log('اولی' + event.pageX)
     X.value = event.pageX;
     drag.value = true;
-    // console.log(X.value)
+    document.getElementById("slider").style.transitionProperty = "none";
 }
 const mouse_moved = (event) => {
-    // if (drag.value) {
-    //     translation.value +=(event.pageX-X.value)/16 ;
-    //     console.log(translation.value+(event.pageX-X.value)/16)
-    //     console.log(event.pageX)
-    // }
+    if (drag.value) {
+        let slider = document.getElementById("slider");
+        let trans = translation.value + (event.pageX - X.value) / 16;
+        if (trans > 0 && trans * 16 + 1400 < slider.clientWidth) {
+            slider.style.transform = "translateX(" + trans + "rem)";
+        }
+    }
 }
 const mouse_upd = (event) => {
+    document.getElementById("slider").style.transitionProperty = "all";
     drag.value = false;
-    console.log('دومی' + event.pageX)
     if (X.value + 200 < event.pageX)
         slider_show('forward');
     else if (event.pageX + 200 < X.value)
@@ -42,8 +44,8 @@ setInterval(slider_show, 3000);
 </script>
 
 <template class="w-full">
-    <div class="mx-auto relative mt-1 rounded-2xl w-[80rem] overflow-hidden">
-        <div id="slider" class=" flex w-fit h-96 transition-all duration-500" @mouseup="mouse_upd"
+    <div class="mx-auto relative mt-1 rounded-2xl w-[80rem] overflow-hidden cursor-pointer">
+        <div id="slider" class=" flex w-fit h-96 duration-500" @mouseup="mouse_upd"
              @mousemove="mouse_moved" @mousedown="mouse_downed"
              :style="`transform: translateX(${translation}rem)`">
             <span class="slider-pages">
@@ -60,7 +62,8 @@ setInterval(slider_show, 3000);
             </span>
         </div>
         <!--        slider controllers-->
-        <div class="flex justify-between items-center w-full px-10 absolute top-40" @mouseup="mouse_upd"
+        <div class="flex justify-between items-center w-full px-10 absolute top-40" @mousemove="mouse_moved"
+             @mouseup="mouse_upd"
              @mousemove.prevent @mousedown="mouse_downed">
             <div class="slider-button" @click="slider_show('backward')">
                 <svg-component name="chev-right" class="size-6"></svg-component>
