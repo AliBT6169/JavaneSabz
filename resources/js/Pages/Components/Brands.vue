bebb08a567b170f05b76d3393e98d2963d4b97f3
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-01 10:22:17 -0800

    set dinamic vision in brands slider

--- resources/js/Pages/Components/Brands.vue
+++ resources/js/Pages/Components/Brands.vue
@@ -1,46 +1,58 @@
 <script setup>
+    import {onMounted} from "vue";
 
+    const props = defineProps(["brandsData"]);
+    onMounted(()=>{
+        console.log(props.brandsData);
+    });
 </script>
 
 <template>
     <div class="w-[80rem] mx-auto overflow-x-scroll">
         <div class="flex w-fit text-xs gap-4 mt-4 py-5">
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/advanced-nutrients.webp" alt="" class="brands-image" @dragstart.prevent>
+            <div class="brand-list" v-for="item in props.brandsData">
+                <img :src="item" alt="" class="brands-image"
+                     @dragstart.prevent>
             </div>
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/agro-zold.webp" alt="" class="brands-image" @dragstart.prevent>
-            </div>
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/ardin.webp" alt="" class="brands-image" @dragstart.prevent>
-            </div>
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/argeto.webp" alt="" class="brands-image" @dragstart.prevent>
-            </div>
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/ariashimi.webp" alt="" class="brands-image" @dragstart.prevent>
-            </div>
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/asia-seed.webp" alt="" class="brands-image" @dragstart.prevent>
-            </div>
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/baghbantak.webp" alt="" class="brands-image" @dragstart.prevent>
-            </div>
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/barenbrug.webp" alt="" class="brands-image" @dragstart.prevent>
-            </div>
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/behcam.webp" alt="" class="brands-image" @dragstart.prevent>
-            </div>
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/behco.webp" alt="" class="brands-image" @dragstart.prevent>
-            </div>
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/biobizz.webp" alt="" class="brands-image" @dragstart.prevent>
-            </div>
-            <div class="brand-list">
-                <img src="../../../../public/images/brands/continental-semences.webp" alt="" class="brands-image" @dragstart.prevent>
-            </div>
+<!--            <div class="brand-list">-->
+<!--                <img src="../../../../public/images/brands/agro-zold.webp" alt="" class="brands-image"-->
+<!--                     @dragstart.prevent>-->
+<!--            </div>-->
+<!--            <div class="brand-list">-->
+<!--                <img src="../../../../public/images/brands/ardin.webp" alt="" class="brands-image" @dragstart.prevent>-->
+<!--            </div>-->
+<!--            <div class="brand-list">-->
+<!--                <img src="../../../../public/images/brands/argeto.webp" alt="" class="brands-image" @dragstart.prevent>-->
+<!--            </div>-->
+<!--            <div class="brand-list">-->
+<!--                <img src="../../../../public/images/brands/ariashimi.webp" alt="" class="brands-image"-->
+<!--                     @dragstart.prevent>-->
+<!--            </div>-->
+<!--            <div class="brand-list">-->
+<!--                <img src="../../../../public/images/brands/asia-seed.webp" alt="" class="brands-image"-->
+<!--                     @dragstart.prevent>-->
+<!--            </div>-->
+<!--            <div class="brand-list">-->
+<!--                <img src="../../../../public/images/brands/baghbantak.webp" alt="" class="brands-image"-->
+<!--                     @dragstart.prevent>-->
+<!--            </div>-->
+<!--            <div class="brand-list">-->
+<!--                <img src="../../../../public/images/brands/barenbrug.webp" alt="" class="brands-image"-->
+<!--                     @dragstart.prevent>-->
+<!--            </div>-->
+<!--            <div class="brand-list">-->
+<!--                <img src="../../../../public/images/brands/behcam.webp" alt="" class="brands-image" @dragstart.prevent>-->
+<!--            </div>-->
+<!--            <div class="brand-list">-->
+<!--                <img src="../../../../public/images/brands/behco.webp" alt="" class="brands-image" @dragstart.prevent>-->
+<!--            </div>-->
+<!--            <div class="brand-list">-->
+<!--                <img src="../../../../public/images/brands/biobizz.webp" alt="" class="brands-image" @dragstart.prevent>-->
+<!--            </div>-->
+<!--            <div class="brand-list">-->
+<!--                <img src="../../../../public/images/brands/continental-semences.webp" alt="" class="brands-image"-->
+<!--                     @dragstart.prevent>-->
+<!--            </div>-->
         </div>
     </div>
 </template>
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -4,12 +4,26 @@ import Header from "@/Pages/Parts/Header.vue";
 import Footer from "@/Pages/Parts/Footer.vue";
 import Slider1 from "@/Pages/Components/Slider1.vue";
 import Brands from "@/Pages/Components/Brands.vue";
+import {ref} from "vue";
+
+const brandsData=ref([
+    "../../../../images/brands/advanced-nutrients.webp",
+    "../../../../images/brands/ardin.webp",
+    "../../../../images/brands/argeto.webp",
+    "../../../../images/brands/ariashimi.webp",
+    "../../../../images/brands/asia-seed.webp",
+    "../../../../images/brands/baghbantak.webp",
+    "../../../../images/brands/barenbrug.webp",
+    "../../../../images/brands/behcam.webp",
+    "../../../../images/brands/biobizz.webp",
+    "../../../../images/brands/continental-semences.webp.webp",
+]);
 </script>
 
 <template class="">
     <Header></Header>
     <slider1></slider1>
-    <Brands class="mb-96"></Brands>
+    <Brands class="mb-96" :brands-data="brandsData"></Brands>
     <Footer></Footer>
 </template>
 <style>
