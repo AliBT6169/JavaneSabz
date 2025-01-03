fcb9c2432ecb020fc40a1e433bd87797fdb6b71b
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-03 08:49:16 -0800

    product making

--- resources/js/Pages/Components/Product.vue
+++ resources/js/Pages/Components/Product.vue
@@ -1,15 +1,24 @@
 <script setup>
 
+import SvgComponent from "@/Components/svg-component.vue";
 </script>
 
 <template>
-    <div class="flex w-fit rounded-lg shadow-sm hover:shadow-lg hover:shadow-slate-500 duration-300 cursor-pointer px-2 bg-defaultColor6 py-4 flex-col items-center gap-4">
+    <div class="flex w-fit rounded-lg shadow-sm hover:shadow-lg hover:shadow-slate-500 duration-300 cursor-pointer px-4 bg-defaultColor6 py-6 flex-col items-center gap-4">
 <!--        image-->
         <div class="size-40 rounded-lg overflow-hidden">
             <img src="../../../../public/images/products/Emamectin.jpg" alt="" class="size-full">
         </div>
-        <div class="flex justify-center w-40 text-xs items-center">
-            <h3 class="">امامکتین بنزوات + ایندوکساکارب ۹% سوسپانسیون</h3>
+<!--        name-->
+        <div class="flex justify-center w-40 items-center">
+            <h3 class="">امامکتین بنزوات</h3>
+        </div>
+<!--        price & buy cart-->
+        <div class="w-40 flex justify-between items-center">
+            <span class="text-defaultColor">800,000 تومان</span>
+            <span class="hover:translate-x-2 duration-500 hover:text-defaultColor">
+                <svg-component name="cart" class="size-6"></svg-component>
+            </span>
         </div>
     </div>
 </template>
