f68a3f5de3d652e22ba9f5e8a5566b62deae5d3c
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-06 08:22:11 -0800

    setting header responsivation on mobile

--- resources/js/Pages/Components/navigation.vue
+++ resources/js/Pages/Components/navigation.vue
@@ -4,7 +4,7 @@ import SvgComponent from "@/Components/svg-component.vue";
 </script>
 
 <template>
-    <div class="mx-auto sticky top-2 z-10">
+    <div class="mx-auto sticky top-2 z-10 hidden md:block">
         <div
             class=" w-full px-6 flex justify-between items-center rounded-full z-10 rounded-tl-md bg-defaultColor h-14 text-slate-100">
 
@@ -125,5 +125,6 @@ import SvgComponent from "@/Components/svg-component.vue";
             </div>
         </div>
     </div>
+    <div class=""></div>
 </template>
 
