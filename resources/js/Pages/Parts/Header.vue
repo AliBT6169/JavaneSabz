bd3a729d71f9156ce03cc9dd3ca8a27197eb3bad
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-06 06:45:55 -0800

    header full responsived

--- resources/js/Pages/Parts/Header.vue
+++ resources/js/Pages/Parts/Header.vue
@@ -22,10 +22,10 @@ const toggleDark = useToggle(isDark);
                 </div>
             </div>
             <!--            search box-->
-            <div class="relative -mt-10 xl:w-[30%] w-[300px] hidden md:block">
+            <div class="relative -mt-10 xl:w-[20rem] w-[300px] md:w-[200px] hidden md:block">
                 <div class="absolute">
                     <input type="text"
-                           class="focus:outline-none focus:ring-transparent transition-colors duration-300 focus:border-defaultColor xl:w-[20rem] sm:[200px] w-[250px] h-10 rounded-2xl bg-gray-50 border-gray-200 dark:bg-gray-600 dark:focus:border-defaultColor5 dark:text-gray-100
+                           class="focus:outline-none focus:ring-transparent transition-colors duration-300 focus:border-defaultColor xl:w-[20rem] sm:[200px] w-[250px] md:w-[150px] h-10 rounded-2xl bg-gray-50 border-gray-200 dark:bg-gray-600 dark:focus:border-defaultColor5 dark:text-gray-100
                                  text-xs lg:text-sm" placeholder="جستجوی محصول">
                     <div class="absolute top-2.5 left-2 cursor-pointer text-slate-200">
                         <svg-component name="search" class="size-5"></svg-component>
