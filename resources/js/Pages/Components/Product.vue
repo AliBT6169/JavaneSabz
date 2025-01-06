256f0419bd677751e4a542b87353b41957b8689b
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-06 05:22:07 -0800

    dark mode is full supported

--- resources/js/Pages/Components/Product.vue
+++ resources/js/Pages/Components/Product.vue
@@ -6,7 +6,7 @@ const props = defineProps(["image", "name" ,"special"]);
 </script>
 
 <template>
-    <div class="relative group overflow-hidden flex w-fit rounded-lg shadow-sm hover:shadow-lg hover:shadow-slate-500 duration-300 cursor-pointer px-4 bg-defaultColor6 py-6 flex-col items-center gap-4">
+    <div class="relative group overflow-hidden flex w-fit rounded-lg shadow-sm hover:shadow-md hover:shadow-slate-500 duration-300 cursor-pointer px-4 bg-defaultColor6 py-6 flex-col items-center gap-4 dark:bg-defaultColor7 dark:shadow-defaultColor">
 <!--        image-->
         <div class="size-40 rounded-lg overflow-hidden">
             <img :src="props.image" alt="" @dragstart.prevent class="size-full">
@@ -17,17 +17,17 @@ const props = defineProps(["image", "name" ,"special"]);
         </div>
 <!--        price & buy cart-->
         <div class="w-40 flex justify-between items-center">
-            <span class="text-defaultColor">800,000 تومان</span>
-            <span class="hover:translate-x-2 duration-500 hover:text-defaultColor">
+            <span class="text-defaultColor dark:text-defaultColor5">800,000 تومان</span>
+            <span class="hover:translate-x-2 duration-500 hover:text-defaultColor dark:hover:text-defaultColor5">
                 <svg-component name="cart" class="size-6"></svg-component>
             </span>
         </div>
 <!--        like & show-->
-        <div class="absolute top-0 bg-defaultColor4 p-2 -left-10 flex flex-col duration-500 items-center group-hover:left-0 rounded-lg">
-            <svg-component title="افزودن به علاقه مندی" name="like" class="size-6 hover:fill-red-500 fill-white text-red-500 duration-500"></svg-component>
+        <div class="absolute top-0 bg-defaultColor4 p-2 -left-10 flex flex-col duration-500 items-center group-hover:left-0 rounded-lg dark:bg-defaultColor7 dark:bg-opacity-50">
+            <svg-component title="افزودن به علاقه مندی" name="like" class="size-6 hover:fill-red-500 fill-white text-red-500 duration-500 dark:fill-defaultColor7 dark:hover:fill-red-900 dark:text-red-900"></svg-component>
             <svg-component title="مشاهده محصول" name="show" class="size-6 hover:text-defaultColor duration-500"></svg-component>
         </div>
-        <div class="absolute top-0 right-0 py-5 rounded-b-full bg-red-600 text-slate-200" v-if="props.special===true">
+        <div class="absolute top-0 right-0 py-5 rounded-b-full bg-red-600 text-slate-200 dark:bg-red-900" v-if="props.special===true">
             <p class="-rotate-90 mr-0.5">ویژه</p>
         </div>
     </div>
