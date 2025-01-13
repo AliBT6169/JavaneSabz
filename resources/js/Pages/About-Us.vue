d02fc9f1e9fedbd4d2dc45e1483e1e8dd3db039f
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-13 11:33:17 -0800

    some changes on abput us page

--- resources/js/Pages/About-Us.vue
+++ resources/js/Pages/About-Us.vue
@@ -12,15 +12,15 @@ import SvgComponent from "@/Components/svg-component.vue";
         <Header></Header>
         <Navigation></Navigation>
         <!--        page location-->
-        <div class="w-full flex ">
-            <div class="flex items-center w-fit gap-2 px-4 border-b-2 border-defaultColor">
-                <span class="hover:text-defaultColor5 duration-500 cursor-pointer">خانه </span>
+        <div class="w-full flex">
+            <div class="flex items-center w-fit gap-2 px-4">
+                <span class="hover:text-defaultColor5 duration-500 cursor-pointer border-b-2 border-defaultColor">خانه </span>
                 <svg-component name="next" class="size-3"></svg-component>
-                <span class="hover:text-defaultColor5 duration-500 cursor-pointer">درباره ما</span>
+                <span class="hover:text-defaultColor5 duration-500 cursor-pointer border-b-2 border-defaultColor">درباره ما</span>
             </div>
         </div>
-        <div class="flex flex-col w-full">
-            <span class="w-full mx-auto p-5 rounded-2xl bg-gradient-to-b from-defaultColor2 to-defaultColor5 2xl:p-10 2xl:w-2/3">
+        <div class="flex flex-col mx-auto items-center w-full">
+            <span class="w-full p-5 rounded-2xl bg-gradient-to-b from-defaultColor2 to-defaultColor5 2xl:p-10 dark:from-50%">
                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و
                 متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و
                 کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده،
@@ -36,6 +36,16 @@ import SvgComponent from "@/Components/svg-component.vue";
                 راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
                 پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
             </span>
+            <div class="flex w-full justify-center gap-4 mt-4 flex-col md:flex-row">
+                <div class="relative w-full h-96 flex justify-center items-center rounded-xl p-5 bg-defaultColor5 mx-auto overflow-hidden dark:bg-defaultColor7">
+                    <img src="../../../public/images/about-us/mojtaba.jpg" alt="" class="size-full rounded-2xl">
+                    <div class="absolute bottom-8 p-2 rounded-lg bg-defaultColor5 dark:bg-defaultColor7 dark:text-white animate-bounce">فروشگاه جوانه سبز از نمای نزدیک</div>
+                </div>
+                <div class="relative w-full h-96 flex justify-center items-center rounded-xl p-5 bg-defaultColor5 mx-auto overflow-hidden dark:bg-defaultColor7">
+                    <img src="../../../public/images/about-us/owner.jpg" alt="" class="size-full rounded-2xl">
+                    <div class="absolute bottom-8 p-2 rounded-lg bg-defaultColor5 dark:bg-defaultColor7 dark:text-white animate-bounce">مدیریت : مجتبی میرقربانی</div>
+                </div>
+            </div>
         </div>
         <Footer></Footer>
     </div>
