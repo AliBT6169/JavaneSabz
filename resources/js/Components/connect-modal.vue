1395f459b390e5eb8a99d2d7442bd86e41f5a494
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-14 04:30:45 -0800

    fix connect-us animation

--- resources/js/Components/connect-modal.vue
+++ resources/js/Components/connect-modal.vue
@@ -1,6 +1,19 @@
 <script setup>
+import {onMounted, ref} from "vue";
+
+const connectUsFormVisibility = ref(false);
+onMounted(() => {
+    setTimeout(()=>{
+        connectUsFormVisibility.value = true;
+        console.log(connectUsFormVisibility.value)
+    },100)
+});
 </script>
 
 <template class="relative">
-    <div class="absolute top-14 -right-40 size-96 rounded-xl bg-slate-800 bg-opacity-50"></div>
+    <div class="invisible opacity-0 absolute top-14 -right-40 size-96 rounded-xl bg-slate-800 lg:bg-opacity-50
+     dark:bg-slate-50 lg:dark:bg-opacity-50 lg:dark:hover:bg-opacity-100 transition-all duration-500 lg:hover:bg-opacity-100"
+     :class="{'!visible !opacity-100':connectUsFormVisibility}">
+
+    </div>
 </template>
