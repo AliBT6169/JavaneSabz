d4c0a9f87287a2054d179fb1616f2e1a2c5becea
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-17 12:43:01 -0800

    compliting connect with us

--- resources/js/Pages/Components/Form/Input-BT1.vue
+++ resources/js/Pages/Components/Form/Input-BT1.vue
@@ -5,10 +5,14 @@ const props = defineProps(["labelText","multiLine"]);
 </script>
 
 <template>
-    <div class="w-full h-fit text-sm justify-between items-center text-nowrap flex gap-2 text-white dark:text-black">
+    <div class="w-full h-fit text-sm justify-between items-center text-nowrap flex gap-2
+     text-white dark:text-black">
         <label class="w-full" for="name">{{ props.labelText }}</label>
         <input id="name" type="text"
-               class="text-sm rounded-xl w-full bg-slate-200 dark:bg-gray-700 text-slate-800 dark:text-white"
-               :class="{'md:h-28': props.multiLine}">
+               class="text-sm rounded-xl w-full bg-slate-200 dark:bg-gray-700
+                text-slate-800 dark:text-white"
+               v-if="!props.multiLine">
+        <textarea v-if="props.multiLine" class="text-sm rounded-xl w-full bg-slate-200 dark:bg-gray-700
+                text-slate-800 dark:text-white" cols="30" rows="5"></textarea>
     </div>
 </template>
--- resources/js/Pages/Components/connect-modal.vue
+++ resources/js/Pages/Components/connect-modal.vue
@@ -15,9 +15,9 @@ onMounted(() => {
 <template>
     <div
         class="h-screen w-screen fixed z-20 top-0 left-0 bg-slate-800 bg-opacity-50 flex items-center justify-center">
-        <div class="invisible opacity-0 -translate-y-96 size-72 rounded-xl bg-slate-800 lg:bg-opacity-50
-        dark:bg-slate-500 lg:dark:bg-opacity-50 lg:dark:hover:bg-opacity-100 transition-all duration-500
-         lg:hover:bg-opacity-100 md:size-96"
+        <div class="invisible opacity-0 border-2 border-defaultColor5 -translate-y-96 h-fit w-72 rounded-xl bg-slate-800 lg:bg-opacity-50
+        dark:bg-slate-500 lg:dark:bg-opacity-50 py-4 lg:dark:hover:bg-opacity-100 transition-all duration-500
+         lg:hover:bg-opacity-100 md:w-96"
              :class="{'!visible !translate-y-0 !opacity-100':connectUsFormVisibility}">
             <form class="size-full p-4 items-center gap-2 flex flex-col" @click.stop>
                 <div class="text-lg font-bold text-defaultColor5 border-x-4 rounded-2xl p-1 border-defaultColor">برای ما
