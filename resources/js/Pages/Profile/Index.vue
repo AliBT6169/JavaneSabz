bb101549c6081e570faaa6b16421b16a3648bb20
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-24 14:23:06 -0800

    profile panel index alignment

--- resources/js/Pages/Profile/Index.vue
+++ resources/js/Pages/Profile/Index.vue
@@ -1,8 +1,8 @@
 <script setup>
 
-import Navigation from "@/Pages/Components/navigation.vue";
 import PanelSideBar from "@/Pages/Profile/Parts/PanelSideBar.vue";
 import PanelHeader from "@/Pages/Profile/Parts/PanelHeader.vue";
+import {profileSidebar} from "@/Pages/Components/Helper/Helper.js";
 </script>
 
 <template>
@@ -11,5 +11,7 @@ import PanelHeader from "@/Pages/Profile/Parts/PanelHeader.vue";
     <div class="mx-auto flex gap-10 items-center w-[20rem]
         sm:w-[30rem] md:w-[40rem] lg:w-[60rem] xl:w-[80rem]">
         <PanelSideBar></PanelSideBar>
+<!--        index pages-->
+        <div class="w-full h-96 bg-defaultColor5 m-auto dark:bg-defaultColor lg:mr-20"></div>
     </div>
 </template>
--- resources/js/Pages/Profile/Parts/PanelSideBar.vue
+++ resources/js/Pages/Profile/Parts/PanelSideBar.vue
@@ -5,9 +5,9 @@ import SvgComponent from "@/Pages/Components/svg-component.vue";
 import SidebarItem from "@/Pages/Components/Panel/SidebarItem.vue";
 </script>
 
-<template @click.prevent>
-    <div class="invisible overflow-scroll no-scroll opacity-0 grid fixed duration-300 top-20 w-20 h-[calc(100vh-5.4rem)] rounded-full bg-defaultColor5
-    dark:bg-defaultColor right-2 py-10 sm:visible sm:opacity-100 sm:gap-6" :class="{'w-40 !rounded-[40px] sm:w-80 !opacity-100 !visible':profileSidebar}">
+<template class="select-none">
+    <div class="invisible overflow-scroll shadow no-scroll opacity-0 grid fixed duration-300 top-20 w-20 h-[calc(100vh-5.4rem)] rounded-full bg-defaultColor5
+    shadow-slate-600 dark:shadow-slate-500 dark:bg-defaultColor right-2 py-10 sm:visible sm:opacity-100 sm:gap-6" :class="{'w-40 !rounded-[40px] sm:w-80 !opacity-100 !visible':profileSidebar}">
         <SidebarItem svg="eye">مشاهده پروفایل</SidebarItem>
         <SidebarItem svg="cart">سبد خرید</SidebarItem>
         <SidebarItem svg="card">تراکنش ها</SidebarItem>
