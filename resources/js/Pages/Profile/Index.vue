f70675daab89ba51dd7e4e77b2b8f134ecfa8d84
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-24 13:53:50 -0800

    indexing some dependensies

--- resources/js/Pages/Profile/Index.vue
+++ resources/js/Pages/Profile/Index.vue
@@ -5,10 +5,10 @@ import PanelSideBar from "@/Pages/Profile/Parts/PanelSideBar.vue";
 import PanelHeader from "@/Pages/Profile/Parts/PanelHeader.vue";
 </script>
 
-<template class="">
+<template>
     <PanelHeader></PanelHeader>
     <div class="mb-20"></div>
-    <div class="mx-auto flex flex-col gap-10 items-center w-[20rem]
+    <div class="mx-auto flex gap-10 items-center w-[20rem]
         sm:w-[30rem] md:w-[40rem] lg:w-[60rem] xl:w-[80rem]">
         <PanelSideBar></PanelSideBar>
     </div>
