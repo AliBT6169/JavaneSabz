3a10172683939548af6874591681047c808ffa6e
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-27 03:24:45 -0800

    index profile show in panell design

--- resources/js/Pages/Profile/Index.vue
+++ resources/js/Pages/Profile/Index.vue
@@ -11,9 +11,32 @@ import {profileSidebar} from "@/Pages/Components/Helper/Helper.js";
     <div class="px-5 sm:pr-28">
         <PanelSideBar></PanelSideBar>
         <!--        index pages-->
-        <div class="w-full h-96 duration-300 bg-defaultColor5 dark:bg-defaultColor"
+        <div class="w-full duration-300 bg-defaultColor5 dark:bg-defaultColor p-5 rounded-2xl"
              :class="{'lg:mr-60 lg:w-[calc(100%-15rem)]':profileSidebar}">
-            <div class=""></div>
+            <div class="*:mb-5">
+                <img src="../../../../public/images/about-us/owner.jpg"
+                     class="ring-2 mx-auto rounded-full w-32 ring-offset-8 ring-offset-defaultColor5
+                     ring-defaultColor dark:ring-defaultColor5 dark:ring-offset-defaultColor"
+                     alt="profile-picture">
+                <div class="grid gap-4 place-content-center text-gray-700 dark:text-gray-300">
+                    <div class="border-b border-gray-700 dark:border-gray-300">
+                        <span class="pl-2 text-black">نام کاربری :</span>
+                        <span>mojtaba-ghorbani</span>
+                    </div>
+                    <div class="border-b border-gray-700 dark:border-gray-300">
+                        <span class="pl-2 text-black">نام و نام خانوادگی :</span>
+                        <span>مجتبی قربانی</span>
+                    </div>
+                    <div class="border-b border-gray-700 dark:border-gray-300">
+                        <span class="pl-2 text-black">جنسیت :</span>
+                        <span>مرد</span>
+                    </div>
+                    <div class="border-b border-gray-700 dark:border-gray-300">
+                        <span class="pl-2 text-black">آدرس :</span>
+                        <span>ایران - مازندران - بهنمیر - بلوار درزیان - کیلومتر 4 - روستای گالشکلا</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
