04b00b6039c34203a596bb31d89bd4d5ea274a74
AliBT6169 <babatabaralireza0@gmail.com>
2025-01-28 01:12:56 -0800

    making transaction items for mobile

---
+++ resources/js/Pages/Components/Panel/TransactionItems.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import SvgComponent from "@/Pages/Components/svg-component.vue";
+</script>
+
+<template>
+    <div class="flex w-full items-center justify-between p-2 rounded-lg border border-gray-500 bg-defaultColor
+         text-white text-xs">
+        <!--            success-->
+        <div class="bg-defaultColor5 bg-opacity-70 p-2 rounded-full">موفق</div>
+        <span class="h-8 bg-slate-400 rounded-full w-0.5"></span>
+        <!--            date & time-->
+        <div class="p-2 rounded-full border">1403/03/03</div>
+        <!--            price-->
+        <div class="rounded-full border border-amber-300 p-2 text-yellow-500 bg-amber-400 bg-opacity-50 *:pl-1"><span>100,000</span>تومان</div>
+        <!--            show more button-->
+        <svg-component name="arrow-left" class="size-5 text-defaultColor2 cursor-pointer"></svg-component>
+    </div>
+</template>
--- resources/js/Pages/Profile/Pages/Transaction.vue
+++ resources/js/Pages/Profile/Pages/Transaction.vue
@@ -1,7 +1,13 @@
 <script setup>
 
+import SvgComponent from "@/Pages/Components/svg-component.vue";
+import TransactionItems from "@/Pages/Components/Panel/TransactionItems.vue";
 </script>
 
 <template>
-    <div class="text-center text-2xl ">تراکنشی موجود نیست...</div>
+    <!--    container-->
+    <div class="grid gap-4 place-items-center">
+        <!--        item-->
+        <TransactionItems v-for="items in 10"></TransactionItems>
+    </div>
 </template>
