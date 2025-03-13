71f9484f53f2aef1687e9bdfc1d6fbf623873ffc
AliBT6169 <babatabaralireza0@gmail.com>
2025-03-13 12:17:34 -0700

    produc show design to price

--- resources/js/Pages/ProductShow.vue
+++ resources/js/Pages/ProductShow.vue
@@ -25,7 +25,7 @@ console.log(props.product.data);
         sm:w-[30rem] md:w-[40rem] lg:w-[60rem] xl:w-[80rem]">
         <Header/>
         <Navigation/>
-        <div class="w-full space-y-6 pb-96 text-defaultColor dark:text-defaultColor5">
+        <div class="w-full space-y-6 pb-96 text-defaultColor dark:text-defaultColor5 md:text-xl">
             <!--        page location-->
             <div class="w-full flex">
                 <div class="flex items-center w-fit gap-2">
@@ -58,16 +58,58 @@ console.log(props.product.data);
                             </template>
                         </Carousel>
                     </div>
-<!--                    size & rate-->
-                    <div class="flex gap-4 *:w-full">
-<!--                        size-->
-                        <div class="p-1 rounded-lg bg-defaultColor5/50 text-red-600 border border-defaultColor/50">
-                            <strong class="text-defaultColor dark:text-defaultColor5">سایز: </strong>
-                            {{product.data.value}}
+                    <!--                    size & rate-->
+                    <div class="flex gap-4 *:w-full md:*:h-12 *:justify-center *:flex *: *:items-center">
+                        <!--                        size-->
+                        <div class="productShowItems text-red-600">
+                            <strong class="text-defaultColor dark:text-defaultColor5 pl-2">سایز : </strong>
+                            {{ product.data.value }}
                         </div>
-<!--                        rate-->
-                        <div class="p-1 rounded-lg bg-defaultColor5/50 text-red-600 border border-defaultColor/50">
-
+                        <!--                        rate-->
+                        <div
+                            class="text-yellow-500 productShowItems">
+                            <strong class="text-defaultColor dark:text-defaultColor5">امتیاز: </strong>
+                            <div class="flex *:size-4 *:stroke-1 *:stroke-yellow-50 md:*:size-6">
+                                <svg-component name="star" class="text-yellow-50"/>
+                                <svg-component v-for="item in 4" name="star" class=""/>
+                            </div>
+                        </div>
+                    </div>
+                    <!--                    category & brand-->
+                    <div class="productShowItems">
+                        <div class="w-full flex justify-between px-4">
+                            <h2 class="">
+                                <strong class="">دسته بندی :</strong>
+                                {{ product.data.category.name }}
+                            </h2>
+                            <div class="h-6 w-0.5 rounded-full bg-defaultColor"></div>
+                            <h2 class="">
+                                <strong class="">برند :</strong>
+                                {{ product.data.brand.name }}
+                            </h2>
+                        </div>
+                    </div>
+                    <!--                    price & quantity-->
+                    <div class="w-full flex gap-4">
+                        <!--                        price-->
+                        <div class="productShowItems w-[60%] p-0 overflow-hidden justify-between">
+                            <strong class="pr-1">قیمت: </strong>
+                            <div class="">
+                                <div class="text-green-600">{{ product.data.sale_price }}</div>
+                                <div class="text-gray-600 line-through">{{ product.data.price }}</div>
+                            </div>
+                            <div
+                                class="bg-red-600 w-1/4 text-center text-lg font-bold h-full py-3 md:text-2xl md:w-1/5">
+                                {{ product.data.off_sale }}%
+                            </div>
+                        </div>
+                        <!--                        quantity-->
+                        <div class="productShowItems w-[40%] justify-between px-3 ">
+                            <strong>موجودی: </strong>
+                            <strong
+                                :class="{'text-red-600':product.data.quantity<20, 'text-yellow-500':product.data.quantity<100&&product.data.quantity>20}">{{
+                                    product.data.quantity
+                                }}</strong>
                         </div>
                     </div>
                 </div>
