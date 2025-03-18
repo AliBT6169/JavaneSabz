2da6abf17f74f37be069342b6b8b0834d1e8053e
AliBT6169 <babatabaralireza0@gmail.com>
2025-03-18 04:56:09 -0700

    setting show search data count

--- resources/js/Pages/Components/Helper/searchStore.js
+++ resources/js/Pages/Components/Helper/searchStore.js
@@ -2,16 +2,19 @@ import {defineStore} from "pinia";
 
 export const useSearchStore = defineStore('search', {
     state: () => ({
-        Products: null,
+        allProducts: null,
+        ProductsToShow: null,
         categories: null,
         brands: null,
         attributes: null,
     }),
     actions: {
         async setData(input) {
-            await axios(route('ProductSearch',input)).then((response) => {
-                this.Products = response.data;
-            }).catch((error)=>{
+            await axios(route('ProductSearch', input)).then((response) => {
+                this.allProducts = response.data;
+                this.ProductsToShow = response.data.slice(0,11);
+                console.log(this.ProductsToShow)
+            }).catch((error) => {
                 console.log(error.response.data);
             });
             await console.log(this.Products)
