f60954ad60a7b46377160fb2edec5f38f5a0bcfa
AliBT6169 <babatabaralireza0@gmail.com>
2025-04-10 09:23:56 -0700

    edit admin resource & dashboard resource for showing address

--- app/Http/Resources/Admin/UserResource.php
+++ app/Http/Resources/Admin/UserResource.php
@@ -17,13 +17,14 @@ class UserResource extends JsonResource
 
         return [
             'id' => $this->id,
-            'full_name' => $this->full_name??'',
+            'full_name' => $this->full_name ?? '',
             'user_name' => $this->name,
             'is_admin' => $this->is_admin,
             'gender' => $this->gender,
             'cellphone' => $this->cellphone,
             'email' => $this->email,
-            'address' => $this->address->address ?? '',
+            'address' => $this->address == null ? '' : trans('iranRegions::slug.' . $this->address->city->province->slug) . ' - ' .
+                trans('iranRegions::slug.' . $this->address->city->slug) . ' - ' . $this->address->address,
             'postal_code' => $this->address->postcode ?? '',
         ];
     }
