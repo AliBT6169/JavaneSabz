d9e30afb3bd6626d7e36fc9a9842fdc1e8a67500
AliBT6169 <babatabaralireza0@gmail.com>
2025-04-30 01:44:05 -0700

    fix orders get in useAuthStore file

--- resources/js/Pages/Components/Helper/authStore.js
+++ resources/js/Pages/Components/Helper/authStore.js
@@ -126,7 +126,7 @@ export const useAuthStore = defineStore('auth', {
             console.log()
             axios.post(route('CouponChecker'), {'id': order_id, 'coupon_code': coupon_code}).then((res) => {
                 console.log(res)
-                this.Orders.splice(0, this.Orders.length, ...res.data.data);
+                this.Orders.splice(0, this.Orders.length, ...res.data);
             }).catch((err) => {
                 console.log(err.response.data.message)
                 this.toastMessage('error', err.response.data.message)
