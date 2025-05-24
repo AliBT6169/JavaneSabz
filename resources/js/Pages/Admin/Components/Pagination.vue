c143b43466be42c3ea7969b3b952ee1e6bb16477
AliBT6169 <babatabaralireza0@gmail.com>
2025-05-24 12:24:08 -0700

    fix admin user index pagination

--- resources/js/Pages/Admin/Components/Pagination.vue
+++ resources/js/Pages/Admin/Components/Pagination.vue
@@ -11,19 +11,19 @@ const props = defineProps({
 
 <template>
 
-    <div class="flex items-center justify-start gap-5 py-5 *:px-4 text-sm sm:text-base sm:justify-center
-                *:rounded-xl *:border *:border-gray-700/50 *:bg-adminColor1 *:dark:bg-adminColor3 md:*:px-10 *:py-4">
+    <div class="flex items-center justify-start gap-5 py-5 *:p-2 text-sm sm:text-base sm:justify-center
+                *:rounded-xl *:border *:border-gray-700/50 *:bg-adminColor1 *:dark:bg-adminColor3 md:*:px-10">
         <Link :href="links.first">
-            <svg-component name="d-arrow-left" class="size-5 rotate-180"/>
+            <svg-component name="d-arrow-left" class="size-4 sm:size-5 rotate-180"/>
         </Link>
         <Link :class="{'!bg-opacity-50':links.prev===null}" :href="links.prev">
-            <svg-component name="arrow-left" class="size-5 rotate-180"/>
+            <svg-component name="arrow-left" class="size-4 sm:size-5 rotate-180"/>
         </Link>
         <Link :class="{'!bg-opacity-50':links.next===null}" :href="links.next">
-            <svg-component name="arrow-left" class="size-5"/>
+            <svg-component name="arrow-left" class="size-4 sm:size-5"/>
         </Link>
         <Link :href="links.last">
-            <svg-component name="d-arrow-left" class="size-5"/>
+            <svg-component name="d-arrow-left" class="size-4 sm:size-5"/>
         </Link>
     </div>
 </template>
