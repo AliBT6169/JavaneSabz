da8fd0b3280c236b368357ab3b04c071d595d98e
AliBT6169 <babatabaralireza0@gmail.com>
2025-05-25 11:39:47 -0700

    make admin input

---
+++ resources/js/Pages/Admin/Components/AdminInput.vue
@@ -0,0 +1,20 @@
+<script setup>
+import SvgComponent from "@/Pages/Components/svg-component.vue";
+
+const props = defineProps({
+    name: '',
+    type: 'text',
+    default_value: '',
+    icon: ''
+});
+</script>
+
+<template>
+    <div class="relative">
+        <div class="text-sm px-3">{{name}} :</div>
+        <input :type="type" :placeholder="name" :value="default_value"
+               class="p-3 w-full rounded-lg border-adminColor2 border-2 placeholder-adminColor2 focus:ring-adminColor2
+                focus:border-adminColor2 dark:bg-gray-600 dark:placeholder-adminColor4">
+        <svg-component v-if="icon!==''" :name="icon" class="size-5 absolute left-1 top-4 z-50 text-black"/>
+    </div>
+</template>
--- resources/js/Pages/Admin/pages/Users/edit.vue
+++ resources/js/Pages/Admin/pages/Users/edit.vue
@@ -2,6 +2,7 @@
 import AdminSideBar from "@/Pages/Admin/Components/AdminSideBar.vue";
 import AdminHeader from "@/Pages/Admin/Components/AdminHeader.vue";
 import Layout from "@/Pages/Admin/Components/Layout.vue";
+import AdminInput from "@/Pages/Admin/Components/AdminInput.vue";
 
 const props = defineProps({
     user: null,
@@ -13,6 +14,12 @@ console.log(props.user.data)
     <AdminHeader/>
     <AdminSideBar/>
     <Layout>
-
+        <div class="space-y-6">
+            <img :src="'/'+user.data.user_image" class="cursor-pointer m-auto duration-300 size-40 rounded-full border-4 border-adminColor2
+             dark:border-adminColor3 hover:scale-95" alt="">
+            <div class="">
+                <AdminInput name="نام و نام خانوادگی" :default_value="user.data.full_name"/>
+            </div>
+        </div>
     </Layout>
 </template>
