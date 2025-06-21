e353d408d94532b537288c88cb5e0e90f24fbc0d
Alireza Babatabar <babatabaralireza@gmail.com>
2025-06-21 12:37:42 +0430

    fix pagination warning

--- resources/js/Pages/Admin/Components/Pagination.vue
+++ resources/js/Pages/Admin/Components/Pagination.vue
@@ -8,25 +8,26 @@ const props = defineProps({
     meta: null,
     createLink: '#',
 })
+console.log(props.links.first)
 </script>
 
 <template>
 
     <div class="flex items-center justify-start gap-5 py-5 *:p-2 text-sm sm:text-base sm:justify-center
                 *:rounded-xl *:border *:border-gray-700/50 *:bg-adminColor1 *:dark:bg-adminColor3 md:*:px-10">
-        <Link :href="links.first">
+        <Link v-if="links.prev!==null" :href="links.first">
             <svg-component name="d-arrow-left" class="size-4 sm:size-5 rotate-180"/>
         </Link>
-        <Link :class="{'!bg-opacity-50':links.prev===null}" :href="links.prev">
+        <Link v-if="links.prev!==null" :href="links.prev">
             <svg-component name="arrow-left" class="size-4 sm:size-5 rotate-180"/>
         </Link>
         <Link :href="route(createLink)">
             افزودن
         </Link>
-        <Link :class="{'!bg-opacity-50':links.next===null}" :href="links.next">
+        <Link  v-if="links.next!==null" :href="links.next">
             <svg-component name="arrow-left" class="size-4 sm:size-5"/>
         </Link>
-        <Link :href="links.last">
+        <Link  v-if="links.next!==null" :href="links.last">
             <svg-component name="d-arrow-left" class="size-4 sm:size-5"/>
         </Link>
     </div>
