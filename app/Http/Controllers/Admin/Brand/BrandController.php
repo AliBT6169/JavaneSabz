4bb7cbd6b735311b8ae53217acf4e58bf2faf10d
Alireza Babatabar <babatabaralireza@gmail.com>
2025-06-25 23:29:46 +0430

    using resource for brands index in admin page

--- app/Http/Controllers/Admin/Brand/BrandController.php
+++ app/Http/Controllers/Admin/Brand/BrandController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\Brand;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Admin\Brands\BrandResource;
 use App\Models\Brand;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -11,7 +12,7 @@ class BrandController extends Controller
 {
     public function index()
     {
-        $brands = Brand::latest()->paginate(20);
+        $brands = BrandResource::collection(Brand::latest()->paginate(20));
         return Inertia::render('Admin/pages/Brands/index', ['brands' => $brands]);
     }
 }
--- app/Http/Resources/Admin/Brands/BrandResource.php
+++ app/Http/Resources/Admin/Brands/BrandResource.php
@@ -20,7 +20,7 @@ class BrandResource extends JsonResource
             'name' => $this->name,
             'is_active' => $this->is_active,
             'icon' => $this->icon,
-            'products' => ProductsResource::collection($this->products)
+            'products'=>ProductsResource::collection($this->products)
         ];
     }
 }
