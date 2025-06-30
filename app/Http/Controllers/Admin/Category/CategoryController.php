6b96df4fa5c81a7a5066b269650eca2fb82940d3
Alireza Babatabar <babatabaralireza@gmail.com>
2025-06-30 13:43:46 +0430

    make controller store methode & request for store categories request

--- app/Http/Controllers/Admin/Category/CategoryController.php
+++ app/Http/Controllers/Admin/Category/CategoryController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Categories\CategoryStoreRequest;
 use App\Http\Resources\Admin\Categories\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class CategoryController extends Controller
@@ -19,5 +21,21 @@ class CategoryController extends Controller
     public function create()
     {
         return Inertia::render('Admin/pages/Categories/create');
+    }
+
+    public function store(CategoryStoreRequest $request)
+    {
+        $image = $request->file('image');
+        $lastId = DB::select('SHOW TABLE STATUS LIKE "categories"')[0]->Auto_increment;
+        $URL = '/images/categories/' . $lastId . '.' . $image->getClientOriginalExtension();
+        $image->move(public_path('images/categories/'), $lastId . '.' . $image->getClientOriginalExtension());
+        Category::create([
+            'name' => $request->name,
+            'slug' => $request->name,
+            'description' => $request->decription,
+            'icon' => $URL,
+            'is_active' => $request->is_active,
+        ]);
+        return response()->json(['message' => 'success'], 200);
     }
 }
