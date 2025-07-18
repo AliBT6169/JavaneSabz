1187a7aea0ef431d34c50ed7b75324e2771a55d1
Alireza Babatabar <babatabaralireza@gmail.com>
2025-07-19 00:18:04 +0430

    make select order user modal item & catch user as props in component

--- resources/js/Pages/Admin/pages/Order/SelectOrderUserModal.vue
+++ resources/js/Pages/Admin/pages/Order/SelectOrderUserModal.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {onBeforeUnmount, onMounted, ref} from "vue";
 import AdminOrderProductSelectItem from "@/Pages/Admin/Components/Order/AdminOrderProductSelectItem.vue";
+import SelectOrderUserModalItem from "@/Pages/Admin/pages/Order/SelectOrderUserModalItem.vue";
 
 const props = defineProps({
     modelValue: {
@@ -36,7 +37,7 @@ onBeforeUnmount(() => {
          ref="modal"
          :class="{'fixed z-50 top-20 size-fit md:size-5/6 py-6 overflow-scroll':modal_status}">
         <div :class="{'hidden':!modal_status}" class="flex gap-5 justify-center flex-wrap">
-
+            <SelectOrderUserModalItem v-for="item in users" :user="item"/>
         </div>
         <div @click.stop="modal_status = true" class="flex justify-center items-center cursor-pointer m-auto duration-300
         size-full rounded-xl border-4 border-adminColor2 dark:border-adminColor3 hover:scale-95 overflow-hidden"
---
+++ resources/js/Pages/Admin/pages/Order/SelectOrderUserModalItem.vue
@@ -0,0 +1,9 @@
+<script setup>
+const props = defineProps({
+    user: Object,
+});
+</script>
+
+<template>
+
+</template>
