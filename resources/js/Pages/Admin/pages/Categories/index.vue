33b7f19eff80c79a734abbdada49956c851f6c57
Alireza Babatabar <babatabaralireza@gmail.com>
2025-07-23 03:21:42 +0430

    categories index search maked

--- resources/js/Pages/Admin/pages/Categories/index.vue
+++ resources/js/Pages/Admin/pages/Categories/index.vue
@@ -7,13 +7,20 @@ import SvgComponent from "@/Pages/Components/svg-component.vue";
 import {Link} from "@inertiajs/vue3";
 import {ref} from "vue";
 import CategoryItem from "@/Pages/Admin/Components/CategoryItem.vue";
+import AdminInput from "@/Pages/Admin/Components/AdminInput.vue";
 
 const props = defineProps({
     categories: null,
 });
 const categoryData = ref(props.categories.data);
+const filteredCategories = ref(props.categories.data);
 const categoryDeleted = (id) => {
     categoryData.value = categoryData.value.filter((item) => item.id !== id);
+    filteredCategories.value = categoryData.value.filter((item) => item.id !== id);
+}
+const searchKeyWord = ref("");
+const searchKeyWordChanged = (e) => {
+    filteredCategories.value = categoryData.value.filter((item) => item.name.includes(e));
 }
 </script>
 
@@ -21,8 +28,10 @@ const categoryDeleted = (id) => {
     <AdminHeader/>
     <AdminSideBar/>
     <Layout>
+        <AdminInput v-model="searchKeyWord" class="!m-auto md:w-1/2" name="جستجو"
+                    @update:modelValue="searchKeyWordChanged($event)"/>
         <div class="space-y-5 lg:flex lg:space-y-0 lg:gap-6 lg:flex-wrap lg:items-center">
-            <CategoryItem v-for="item in categoryData" @deleted="categoryDeleted($event)" :category-data="item"/>
+            <CategoryItem v-for="item in filteredCategories" @deleted="categoryDeleted($event)" :category-data="item"/>
             <div class="flex justify-center">
                 <Link :href="route('admin.categories.create')"
                       class="border-2 cursor-pointer border-black rounded-xl duration-300 p-2 hover:scale-95">
