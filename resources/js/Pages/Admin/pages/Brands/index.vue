1cc056652fe4eb7c38d848cef2eee090069cca1e
Alireza Babatabar <babatabaralireza@gmail.com>
2025-07-23 15:56:12 +0430

    make search for brands

--- resources/js/Pages/Admin/pages/Brands/index.vue
+++ resources/js/Pages/Admin/pages/Brands/index.vue
@@ -7,13 +7,20 @@ import SvgComponent from "@/Pages/Components/svg-component.vue";
 import BrandItem from "@/Pages/Admin/Components/BrandItem.vue";
 import {Link} from "@inertiajs/vue3";
 import {ref} from "vue";
+import AdminInput from "@/Pages/Admin/Components/AdminInput.vue";
 
 const props = defineProps({
     brands: null,
 });
 const brandData = ref(props.brands.data);
+const filteredBrands = ref(props.brands.data);
 const brandDeleted = (id) => {
     brandData.value = brandData.value.filter((item) => item.id !== id);
+}
+const searchKeyWord = ref("");
+
+const searchKeyWordChanged = (e) => {
+    filteredBrands.value = brandData.value.filter((item) => item.name.includes(e));
 }
 </script>
 
@@ -21,8 +28,10 @@ const brandDeleted = (id) => {
     <AdminHeader/>
     <AdminSideBar/>
     <Layout>
+        <AdminInput v-model="searchKeyWord" class="!m-auto md:w-1/2" name="جستجو"
+                    @update:modelValue="searchKeyWordChanged($event)"/>
         <div class="grid grid-cols-3 gap-5 sm:grid-cols-4 md:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8">
-            <BrandItem v-for="item in brandData" @deleted="brandDeleted($event)" :brand-data="item"/>
+            <BrandItem v-for="item in filteredBrands" @deleted="brandDeleted($event)" :brand-data="item"/>
             <Link :href="route('admin.brands.create')"
                   class="border-2 cursor-pointer border-black rounded-xl duration-300 p-2 hover:scale-95">
                 <svg-component name="plus" class="size-full"/>
