ad87dec6afdf3ed72c0b20f589f1cd001c528df9
Alireza Babatabar <babatabaralireza@gmail.com>
2025-08-13 16:27:25 +0430

    make pictures for blog create in cms

--- resources/js/Pages/Admin/pages/Blogs/create.vue
+++ resources/js/Pages/Admin/pages/Blogs/create.vue
@@ -7,12 +7,19 @@ import {ref} from "vue";
 import ToastWarning from "@/Pages/Admin/Components/ToastWarning.vue";
 import {useToast} from "vue-toastification";
 import AdminStatusInput from "@/Pages/Admin/Components/AdminStatusInput.vue";
+import AdminTextArea from "@/Pages/Admin/Components/AdminTextArea.vue";
+import AdminPictureInput from "@/Pages/Admin/Components/AdminPictureInput.vue";
+import SvgComponent from "@/Pages/Components/svg-component.vue";
 
 const form = ref({
     title: '',
     status: true,
     description: '',
+    pictures: [
+        '',
+    ],
 });
+
 const sendData = async () => {
     const content = {
         component: ToastWarning,
@@ -34,15 +41,37 @@ const sendData = async () => {
     toast.warning(content);
 
 }
+
+const addImage = () => {
+    form.value.pictures.push('');
+}
+
+const onPictureDelete = (index) => {
+    form.value.pictures.splice(index, 1);
+    console.log(form.value.pictures);
+}
 </script>
 
 <template>
     <Layout>
         <form @submit.prevent="sendData">
             <div
-                class="space-y-6 py-4 *:w-full *:h-fit md:space-y-0 md:flex md:flex-wrap md:items-center md:justify-center md:gap-5 *:md:w-[45%]">
-                <admin-input v-model="form.title" name="موضوع"/>
+                class="space-y-6 py-4 ">
+                <admin-input v-model="form.title" class="!w-60" name="موضوع"/>
+                <div class="admin_inputs flex flex-wrap gap-5 justify-center">
+                    <div class="relative flex justify-center items-center overflow-hidden size-40 group"
+                         v-for="(item, index) in form.pictures">
+                        <AdminPictureInput v-model="form.pictures[index]" :my-key="index"/>
+                        <svg-component name="delete"
+                                       @click="onPictureDelete(index)"
+                                       class="size-7 rounded-xl p-1 absolute top-3 md:top-60 border cursor-pointer
+                                       border-current duration-300 bg-gray-800/50 hover:bg-red-500/50 hover:text-red-500 md:group-hover:top-1"/>
+                    </div>
+                    <svg-component name="plus" class="cursor-pointer duration-300 size-40 hover:scale-95"
+                                   @click="addImage"/>
+                </div>
                 <AdminStatusInput name="وضعیت :" v-model="form.status"/>
+                <AdminTextArea v-model="form.description"/>
                 <div class="space-y-2 md:space-y-0 md:flex md:gap-4 md:justify-end md:!w-[90%]">
                     <admin-button type="submit" text="ثبت"/>
                     <Link class="block" :href="route('admin.blogs.index')">
