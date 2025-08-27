7744099b0aff3963a7102fa3c35e9d16947fb638
Alireza Babatabar <babatabaralireza@gmail.com>
2025-08-28 00:38:37 +0430

    finish with banner in crm

--- resources/js/Pages/Admin/pages/Settings/BannerItemSetting.vue
+++ resources/js/Pages/Admin/pages/Settings/BannerItemSetting.vue
@@ -86,28 +86,31 @@ const sendData = async () => {
 
 <template>
     <div
-        class="!w-[5.5rem] flex !justify-center bg-adminColor2 duration-500 transition-all overflow-hidden rounded-xl
+        class="w-48 flex !justify-center bg-adminColor2 duration-500 transition-all overflow-hidden rounded-xl
          border-2 border-current dark:bg-adminColor3"
         ref="modal"
         :class="{'fixed z-50 top-20 no-scrollbar !size-5/6 py-6 overflow-scroll !bg-adminColor1 dark:!bg-adminColor3 left-10':modal_status,' hover:scale-95':!modal_status}">
         <div v-if="!modal_status" @click.stop="modal_status = true" ref="modal"
-             class="size-fit flex justify-center cursor-pointer overflow-hidden items-center px-3 ">
-            {{modelName}}
+             class="size-full flex relative justify-center cursor-pointer overflow-hidden items-center px-3 ">
+            <div class="absolute z-10 top-10 p-1 rounded-xl bg-gray-50/80">{{modelName}}</div>
             <div
-                class="size-[30%] cursor-pointer md:size-60 rounded-xl overflow-hidden border-4 border-transparent duration-300 relative flex justify-center">
+                class="size-48 text-center space-y-2 cursor-pointer rounded-xl overflow-hidden border-4
+                border-transparent duration-300 relative flex justify-center">
                 <img :src="banner.icon" alt="" class="size-full">
                 <div
-                    class="w-1/2 absolute bottom-10 p-1 rounded-xl  text-center bg-gray-50/50  dark:bg-gray-900/50 text-sm">
-                    {{ banner.title }}
+                    class="w-1/2 absolute bottom-10 p-1 rounded-xl  text-center bg-gray-50/80 dark:bg-gray-900/50 text-sm">
+                    {{ attribute!==undefined?attribute.name:'تنظیم نشده' }}
                 </div>
             </div>
         </div>
         <div v-else class="">
-            <div class="p-5 space-y-4">
-                <AttributeItem :choosable="true" :selected="true" v-if="attribute"
-                               @selected="changeAttribute(attribute.id)" :Attribute="attribute"/>
-                <AttributeItem v-for="item in attributes" :choosable="true" :selected="false"
-                               @selected="changeAttribute(item.id)" :Attribute="item"/>
+            <div class="flex justify-center gap-5">
+                <div class="p-5 space-y-4">
+                    <AttributeItem :choosable="true" :selected="true" v-if="attribute"
+                                   @selected="changeAttribute(attribute.id)" :Attribute="attribute"/>
+                    <AttributeItem v-for="item in attributes" :choosable="true"
+                                   @selected="changeAttribute(item.id)" :Attribute="item"/>
+                </div>
             </div>
         </div>
     </div>
--- resources/js/Pages/Admin/pages/Settings/BannersSetting.vue
+++ resources/js/Pages/Admin/pages/Settings/BannersSetting.vue
@@ -7,18 +7,15 @@ const props = defineProps({
     settings: Object,
     is_active: Boolean,
 });
-const attributes = ref(props.settings);
+const banners = ref(props.settings);
 </script>
 
 <template>
     <div :class="{'opacity-0 top-[-1000px] invisible':!is_active}"
          class="adminSettingPagesDesign">
         <div class="flex gap-5 items-center justify-center flex-wrap">
-            <div
-                class="text-center space-y-2"
-                v-for="(item, index) in attributes">
-                <BannerItemSetting :itemId="item.id" :key="'banner'+index" :banner="item" :modelName="'بنر '+(index+1)"/>
-            </div>
+            <BannerItemSetting v-for="(item, index) in banners" :itemId="item.id" :key="'banner'+index" :banner="item"
+                               :modelName="'بنر '+(index+1)"/>
         </div>
     </div>
 </template>
