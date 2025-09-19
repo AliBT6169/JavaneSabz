608873d932c094ee9ff02485db9580b01e5b5b7b
Alireza Babatabar <babatabaralireza@gmail.com>
2025-09-19 21:08:00 +0430

    fix categories bug

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,7 +11,7 @@ class Category extends Model
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
 
-    protected $table = 'Categories';
+    protected $table = 'categories';
     protected $fillable = [
         'id',
         'name',
