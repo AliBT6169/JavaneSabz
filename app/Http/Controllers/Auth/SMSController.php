4b5f8eba1e4b85995766d2d992e04f214636b624
Alireza Babatabar <babatabaralireza@gmail.com>
2025-10-09 20:42:44 +0330

    fixing mobile sms bug

--- app/Http/Controllers/Auth/SMSController.php
+++ app/Http/Controllers/Auth/SMSController.php
@@ -42,7 +42,7 @@ class SMSController extends Controller
         $code = rand(10000, 99999);
         $message = $code . ' ' . '  :کد ورورد شما' . PHP_EOL . 'به سامانه جوانه سبز خوش آمدید' . PHP_EOL . 'لغو=11';
         Cache::put($request->mobile, $code, Date::now()->addMinutes(2));
-        $result = Http::post('https://console.melipayamak.com/api/send/shared/757ea766af2446918fa2bb00086b42a8', [
+        $result = Http::post('https://rest.payamak-panel.com/api/SendSMS/SendSMS', [
             'username' => '19114303905',
             'password' => '#E2@Q',
             'to' => $request->mobile,
