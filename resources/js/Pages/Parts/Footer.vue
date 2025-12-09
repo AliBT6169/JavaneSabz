24fbe2b656aeb5c95c73061d92a20412ce545272
Alireza Babatabar <babatabaralireza@gmail.com>
2025-12-09 12:27:57 +0330

    enamd adding

--- resources/js/Pages/Parts/Footer.vue
+++ resources/js/Pages/Parts/Footer.vue
@@ -2,15 +2,24 @@
 
 import SvgComponent from "@/Pages/Components/svg-component.vue";
 import {Link} from "@inertiajs/vue3";
+import {onMounted, ref} from "vue";
+
 const props = defineProps({
     settings: Object,
+})
+
+const siteDomain = ref();
+onMounted(() => {
+    const siteLocation = window.location.hostname.split(".");
+    siteLocation.value = siteLocation.pop();
 })
 </script>
 
 <template>
     <div
-         class="p-2 bg-defaultColor7 w-full flex justify-center items-center rounded-t-[8rem] overflow-hidden">
-        <div class="pb-40 w-full md:pb-0 items-center md:items-start md:justify-around flex flex-col text-white md:flex-row">
+        class="p-2 bg-defaultColor7 w-full flex justify-center items-center rounded-t-[8rem] overflow-hidden">
+        <div
+            class="pb-40 w-full md:pb-0 items-center md:items-start md:justify-around flex flex-col text-white md:flex-row">
             <div class="flex flex-col gap-2 h-full">
                 <div
                     class="flex duration-300 items-center cursor-pointer gap-2 border-b-2 border-defaultColor5">
@@ -74,6 +83,18 @@ const props = defineProps({
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d205.92511102483456!2d52.77588788202066!3d36.650352865990975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f857d39956460cb%3A0xd35d50893e1ab96f!2z2LPZhdmI2YUg2qnYtNin2YjYsdiy24wg2KzZiNin2YbZhyDYs9io2LIg2LTZhdin2YQ!5e0!3m2!1sen!2s!4v1736097896290!5m2!1sen!2s"
                         width="200" height="150" style="border: none" allowfullscreen="" loading="lazy"
                         referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+            <div class="flex flex-col gap-2 h-full">
+                <div class="flex border-b-2 border-defaultColor5 justify-center items-center">
+                    اعتماد
+                </div>
+                <div class="">
+                    <a v-if="siteDomain === 'com'" referrerpolicy='origin' target='_blank'
+                       href='https://trustseal.enamad.ir/?id=684132&Code=Wt5jPY4YRGXtd0eqqvH07xKke2ZQkncZ'><img
+                        referrerpolicy='origin'
+                        src='https://trustseal.enamad.ir/logo.aspx?id=684132&Code=Wt5jPY4YRGXtd0eqqvH07xKke2ZQkncZ'
+                        alt='' style='cursor:pointer' code='Wt5jPY4YRGXtd0eqqvH07xKke2ZQkncZ'></a>
                 </div>
             </div>
         </div>
